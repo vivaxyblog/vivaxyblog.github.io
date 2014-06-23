f29c1159ee6a69b2f50518120d0cdd000c344b9f
vivaxy <xyxuye2007@126.com>
2014-06-23 21:06:07 +0800

    put ubuntu font on server

--- analysis/ba.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?3b811795239a4f1617947864666de17d";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
