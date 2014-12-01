422686a703a064b5b42ff8748cbb938869279bc3
xuye <stephen.xu@dianping.com>
2014-12-01 10:12:16 +0800

    add posts

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+/**
+ * @since 14/12/1 上午10:02
+ * @author vivaxy
+ */
+var gulp = require('gulp');
+var del = require('del');
+
+var paths = {
+    src: '_site'
+};
+
+gulp.task('clean', function(){
+});
