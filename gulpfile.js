b9f6872abd710e1f0df723635c678c172b585ba6
vivaxy <xyxuye2007@126.com>
2014-12-03 13:33:53 +0800

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,19 +7,20 @@ var del = require('del');
 var shell = require('gulp-shell');
 
 var paths = {
-    src: ['_site/*'],
-    del: ['index.html', 'css', 'img', 'js', '2011', '2014', '2015'],
-    dest: ''
+    clean1: ['index.html', 'css', 'img', 'js', '2011', '2014', '2015'],
+    clean2: ['_site']
 };
 
-gulp.task('move', ['clean'], shell.task('cp -r ./_site/* ./'));
+var cmd = 'cp -r ./_site/* ./';
 
-gulp.task('clean', function(cb){
-    return del(paths.del, cb);
+gulp.task('move', ['clean1'], shell.task(cmd));
+
+gulp.task('clean1', function(cb){
+    return del(paths.clean1, cb);
 });
 
 gulp.task('serve', shell.task('jekyll serve'));
 
 gulp.task('default', ['move'], function(cb){
-    return del(paths.src, cb);
+    return del(paths.clean2, cb);
 });
