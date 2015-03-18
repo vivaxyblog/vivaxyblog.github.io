054a7935aa8fe92cf05a7e3033f4016e020fe1f1
vivaxy <xyxuye2007@126.com>
2015-03-18 13:28:54 +0800

    f2e standard add references

--- gulpfile.js
+++ gulpfile.js
@@ -8,28 +8,28 @@ var shell = require('gulp-shell');
 var fs = require('fs');
 
 var paths = {
-  clean1: ['index.html', 'css', 'image', 'js', '2011', '2014', '2015'],
-  clean2: ['_site']
+    clean1: ['index.html', 'css', 'image', 'js', '2011', '2014', '2015'],
+    clean2: ['_site']
 };
 
-var cmd1 = 'cp -r ./_site/* ./';
-var cmd2 = 'git add image/* js/* css/* 2011/* 2014/*';
+var cmdMove = 'cp -r ./_site/* ./';
+var cmdGit = 'git add .';
 
 gulp.task('clean1', function (cb) {
-  fs.open(paths.clean2[0], 'r', function (err, fd) {
-    if (err) return err;
-    return del(paths.clean1, cb);
-  });
+    fs.open(paths.clean2[0], 'r', function (err, fd) {
+        if (err) return err;
+        return del(paths.clean1, cb);
+    });
 });
 
-gulp.task('move', ['clean1'], shell.task(cmd1));
+gulp.task('move', ['clean1'], shell.task(cmdMove));
 
 gulp.task('clean2', ['move'], function (cb) {
-  return del(paths.clean2, cb);
+    return del(paths.clean2, cb);
 });
 
 /**
  * default task
  */
-gulp.task('default', ['clean2'], shell.task(cmd2));
+gulp.task('default', ['clean2'], shell.task(cmdGit));
 
