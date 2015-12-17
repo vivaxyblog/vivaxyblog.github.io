350a725f81b7b3319ce041e4341dcde0975a8c28
vivaxy <xyxuye2007@126.com>
2015-12-17 10:29:49 +0800

    update copyright

--- js/duoshuo/duoshuo.js
+++ js/duoshuo/duoshuo.js
@@ -1,9 +1,10 @@
-var duoshuoQuery = {short_name:"vivaxy"};
-	(function() {
-		var ds = document.createElement('script');
-		ds.type = 'text/javascript';ds.async = true;
-		ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
-		ds.charset = 'UTF-8';
-		(document.getElementsByTagName('head')[0] 
-		 || document.getElementsByTagName('body')[0]).appendChild(ds);
-	})();
+var duoshuoQuery = {short_name: 'vivaxy'};
+(function () {
+    var ds = document.createElement('script');
+    ds.type = 'text/javascript';
+    ds.async = true;
+    ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+    ds.charset = 'UTF-8';
+    (document.getElementsByTagName('head')[0]
+    || document.getElementsByTagName('body')[0]).appendChild(ds);
+})();
