ed7359c632289a617813f16f2806d3220d5d79d7
vivaxy <xyxuye2007@126.com>
2018-04-08 14:42:42 +0800

    feat(icon): :sparkles:Add more icons

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * @author vivaxy
  */
 
-var CACHE_NAME = 'cache-datetime-1523169404995';
+var CACHE_NAME = 'cache-datetime-1523169746612';
 
 // @see https://davidwalsh.name/service-worker-claim
 self.addEventListener('install', function () {
