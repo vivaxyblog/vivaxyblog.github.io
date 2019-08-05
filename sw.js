2d7f40d4169ba68f0fb05a42de1526312b1b5aa6
vivaxy <xyxuye2007@126.com>
2019-08-05 14:09:17 +0800

    chore(dep): :lock: fix cryptiles and hoek security problems

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * @author vivaxy
  */
 
-var CACHE_NAME = 'cache-datetime-1564985234412';
+var CACHE_NAME = 'cache-datetime-1564985358427';
 
 // @see https://davidwalsh.name/service-worker-claim
 self.addEventListener('install', function() {
