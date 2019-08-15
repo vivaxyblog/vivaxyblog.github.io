13cbb2a88cc064114f04ff7d0787432675780e8e
vivaxy <xyxuye2007@126.com>
2019-08-15 10:06:21 +0800

    style(code): :lipstick: make code font smaller

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * @author vivaxy
  */
 
-var CACHE_NAME = 'cache-datetime-1565796377407';
+var CACHE_NAME = 'cache-datetime-1565834782099';
 
 // @see https://davidwalsh.name/service-worker-claim
 self.addEventListener('install', function() {
