671dae1bf5908b5c5817338649ef9d1bbf9fe29b
vivaxy <xyxuye2007@126.com>
2019-08-15 16:11:15 +0800

    ci(commitlint): :wrench: add commitlint

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * @author vivaxy
  */
 
-var CACHE_NAME = 'cache-datetime-1565835239445';
+var CACHE_NAME = 'cache-datetime-1565856676651';
 
 // @see https://davidwalsh.name/service-worker-claim
 self.addEventListener('install', function() {
