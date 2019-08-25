1c32e256553ecd5c132bea72cb481708a2460af7
vivaxy <xyxuye2007@126.com>
2019-08-25 20:27:03 +0800

    fix(post): :bug: 2016-08-07-webview-prevent-scroll-or-slide-to-close

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * @author vivaxy
  */
 
-var CACHE_NAME = 'cache-datetime-1566656630721';
+var CACHE_NAME = 'cache-datetime-1566736025033';
 
 // @see https://davidwalsh.name/service-worker-claim
 self.addEventListener('install', function() {
