1dd0d5e547fb9b01b0944b7b42bf3375d286973c
vivaxy <xyxuye2007@126.com>
2019-09-02 22:41:48 +0800

    fix(post): :bug:

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * @author vivaxy
  */
 
-var CACHE_NAME = 'cache-datetime-1567003607657';
+var CACHE_NAME = 'cache-datetime-1567435310539';
 
 // @see https://davidwalsh.name/service-worker-claim
 self.addEventListener('install', function() {
