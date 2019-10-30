a7d07ff56eba6ca8227fd609a1ff494ea5954888
vivaxy <xyxuye2007@126.com>
2019-10-30 23:43:11 +0800

    build(gem): :wrench:

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * @author vivaxy
  */
 
-var CACHE_NAME = 'cache-datetime-1569859951403';
+var CACHE_NAME = 'cache-datetime-1572450193787';
 
 // @see https://davidwalsh.name/service-worker-claim
 self.addEventListener('install', function() {
