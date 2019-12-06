418e54ba5a7d1ea2becd2ee9a939c9bc0cd23d65
vivaxy <xyxuye2007@126.com>
2019-12-06 14:52:14 +0800

    feat(atom): :sparkles: update atom content

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * @author vivaxy
  */
 
-var CACHE_NAME = 'cache-datetime-1575524892312';
+var CACHE_NAME = 'cache-datetime-1575615135968';
 
 // @see https://davidwalsh.name/service-worker-claim
 self.addEventListener('install', function() {
