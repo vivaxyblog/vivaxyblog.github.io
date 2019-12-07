76d91b9c6eba3d7d19591cc2c9b1fb304a241bb0
vivaxy <xyxuye2007@126.com>
2019-12-07 15:06:27 +0800

    fix(post): :bug: 2019-12-07-decode-a-png-image-with-javascript-cn

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * @author vivaxy
  */
 
-var CACHE_NAME = 'cache-datetime-1575699690582';
+var CACHE_NAME = 'cache-datetime-1575702389381';
 
 // @see https://davidwalsh.name/service-worker-claim
 self.addEventListener('install', function() {
