2dc2b7ef35b27ec1de37314ce8933cacdf0dc6c9
vivaxy <xyxuye2007@126.com>
2020-05-28 08:44:44 +0800

    docs(about): :pencil: update about me

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * @author vivaxy
  */
 
-var CACHE_NAME = 'cache-datetime-1588246250747';
+var CACHE_NAME = 'cache-datetime-1590626686352';
 
 // @see https://davidwalsh.name/service-worker-claim
 self.addEventListener('install', function() {
