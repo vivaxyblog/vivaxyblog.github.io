337c99c1321b22604e17fa94e659af33e36a3087
vivaxy <xyxuye2007@126.com>
2020-07-23 11:33:54 +0800

    chore: merge

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * @author vivaxy
  */
 
-var CACHE_NAME = 'cache-datetime-1594972751085';
+var CACHE_NAME = 'cache-datetime-1595475234771';
 
 // @see https://davidwalsh.name/service-worker-claim
 self.addEventListener('install', function() {
