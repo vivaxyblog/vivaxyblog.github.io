bde9ba1b6e0195ec0549f978ae1c268a60c0d983
vivaxy <xyxuye2007@126.com>
2021-04-30 13:31:22 +0800

    chore:  update copyright year

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * @author vivaxy
  */
 
-var CACHE_NAME = 'cache-datetime-1618378772289';
+var CACHE_NAME = 'cache-datetime-1619760683299';
 
 // @see https://davidwalsh.name/service-worker-claim
 self.addEventListener('install', function() {
