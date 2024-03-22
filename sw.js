02d4ebcb5c8a93d257b1bb0e0da1fbed97064498
vivaxy <xyxuye2007@126.com>
2024-03-22 10:18:08 +0800

    feat(seo): add bing webmaster html file

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * @author vivaxy
  */
 
-var CACHE_NAME = 'cache-datetime-1711073685428';
+var CACHE_NAME = 'cache-datetime-1711073890289';
 
 // @see https://davidwalsh.name/service-worker-claim
 self.addEventListener('install', function() {
