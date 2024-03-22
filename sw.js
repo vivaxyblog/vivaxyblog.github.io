7fc4449e521923bc3eec8e923ff5abd19ed554b8
vivaxy <xyxuye2007@126.com>
2024-03-22 10:14:44 +0800

    feat(seo): add google search console file

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * @author vivaxy
  */
 
-var CACHE_NAME = 'cache-datetime-1710850408461';
+var CACHE_NAME = 'cache-datetime-1711073685428';
 
 // @see https://davidwalsh.name/service-worker-claim
 self.addEventListener('install', function() {
