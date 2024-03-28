48d56425aced374ecc17539b104982e45f88c0c7
vivaxy <xyxuye2007@126.com>
2024-03-28 15:48:57 +0800

    feat(ads): add google ads

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * @author vivaxy
  */
 
-var CACHE_NAME = 'cache-datetime-1711073890289';
+var CACHE_NAME = 'cache-datetime-1711612138346';
 
 // @see https://davidwalsh.name/service-worker-claim
 self.addEventListener('install', function() {
